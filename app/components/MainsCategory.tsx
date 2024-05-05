aacc02adcbecbeefdb8f1c481211d32b39274624
erDeep640390 <erdeep64930@gmail.com>
2024-05-05 13:29:11 +0530

    heading text has been change

--- app/components/MainsCategory.tsx
+++ app/components/MainsCategory.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 const MainsCategory = () => {
   return (
     <div>
-          <h2 className="bold-40 lg:bold-64">Mains Categories</h2>
+          <h2 className="text-xl text-black">Mains Categories</h2>
        <ul className="mt-10 grid gap-10 md:grid-cols-2 lg:mg-20 lg:gap-20">
             {FEATURES.map((feature) => (
               <FeatureItem 
@@ -30,7 +30,7 @@ const FeatureItem = ({ title, icon, description }: FeatureItem) => {
       <div className="rounded-full p-4 lg:p-7 bg-green-50">
         <Image src={icon} alt="map" width={28} height={28} />
       </div>
-      <h2 className="bold-20 lg:bold-32 mt-5 capitalize">
+      <h2 className="text-xl lg:bold-32 mt-5 capitalize">
         {title}
       </h2>
       <p className="regular-16 mt-5 bg-white/80 text-gray-30 lg:mt-[30px] lg:bg-none">
